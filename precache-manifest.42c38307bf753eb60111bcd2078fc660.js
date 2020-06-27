600ea07e4e5f5df8eeca51f4b302c315bec3f881
Sargares22 <seredkinsp@mail.ru>
2020-06-28 01:06:00 +0700

    Updates

---
+++ precache-manifest.42c38307bf753eb60111bcd2078fc660.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3670c91669eb9b2e096b2f82b9c5aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45a28ce41b799f3ad49",
+    "url": "/static/css/main.9aa4ae74.chunk.css"
+  },
+  {
+    "revision": "c55bddde1a2bcf03d4c5",
+    "url": "/static/js/2.f7f27c82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f7f27c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45a28ce41b799f3ad49",
+    "url": "/static/js/main.ba15e23d.chunk.js"
+  },
+  {
+    "revision": "403a8d20799347486024",
+    "url": "/static/js/runtime-main.46e5ef84.js"
+  },
+  {
+    "revision": "e6c6c6ca9412902c3af5cc29ba3b2cc0",
+    "url": "/static/media/bin.e6c6c6ca.svg"
+  },
+  {
+    "revision": "564beba017f06fe9bc8ae0347887981b",
+    "url": "/static/media/cog.564beba0.svg"
+  }
+]);
